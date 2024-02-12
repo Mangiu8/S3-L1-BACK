4e40faea1e8ff7427dac6a5f8ee460432ec1360f
Antonio Marucci <antoniomarucci13@gmail.com>
2024-02-12 14:56:17 +0100

    Aggiungere i file di progetto.

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+using System.Web.UI;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = ConfigurationManager.AppSettings["Nomecandidato"];
+        }
+    }
+}
